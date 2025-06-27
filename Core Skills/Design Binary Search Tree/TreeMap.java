5093126567d35633d80226d4dcc7f68da8fbcaf3
Steve Eckardt <seckardt@pacbell.net>
2025-06-27 16:37:38 -0700

    Create TreeMap.java

---
+++ Core Skills/Design Binary Search Tree/TreeMap.java
@@ -0,0 +1,44 @@
+class TreeMap {
+	class Node{
+		int index;
+		int value;
+		Node left;
+		Node right;
+		
+		Node(int index, int value){
+			this.index = index;
+			this.value = value;
+			this.left = null;
+			this.right = null;
+		}
+	}
+	
+	
+    public TreeMap() {
+
+    }
+
+    public void insert(int key, int val) {
+
+    }
+
+    public int get(int key) {
+		return -1;
+    }
+
+    public int getMin() {
+		return -1;
+    }
+
+    public int getMax() {
+		return -1;
+    }
+
+    public void remove(int key) {
+       
+    }
+
+    public List<Integer> getInorderKeys() {
+		return new ArrayList<>();
+    }
+}
