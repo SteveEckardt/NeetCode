61a9aaf9dc22f89cf497fa9b57912f6a30d80c45
Steve Eckardt <seckardt@pacbell.net>
2025-07-06 08:54:40 -0700

    begin design graph

---
+++ CoreSkills/Implement Data Structures/Graph.java
@@ -0,0 +1,18 @@
+class Graph {
+
+    public Graph() {
+
+    }
+
+    public void addEdge(int src, int dst) {
+
+    }
+
+    public boolean removeEdge(int src, int dst) {
+
+    }
+
+    public boolean hasPath(int src, int dst) {
+
+    }
+}
